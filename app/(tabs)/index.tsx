bf9acc89edf7e56bc8480844cf78da596380a2d0
Rayane Guemmoud <guemmoudrayane03@gmail.com>
2025-04-16 15:31:01 +0200

    Save

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
-import { Image, StyleSheet, Platform, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
-import { BlurView } from 'expo-blur';
 import { useEffect, useState } from 'react';
-import { fetch } from 'expo/fetch'; 
+import { SafeAreaView } from 'react-native-safe-area-context';
 import ScannedProductOverlay from '@/components/ScannedProductOverlay';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function HomeScreen() {
   const [permission, requestPermission] = useCameraPermissions();
   const [scanned, setScanned] = useState(false);
   const [barcodeData, setBarcodeData] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!permission) return;
+    if (!permission.granted || ['undetermined', 'denied'].includes(permission.status)) {
+      requestPermission()
+    }
+  });
 
   if (!permission) {
     return (
@@ -20,7 +27,7 @@ export default function HomeScreen() {
 
   if (!permission.granted) {
     return (
-      <View>
+      <View style={styles.container2}>
         <Text>Camera permission is not granted</Text>
         <Button title="Request permission" onPress={requestPermission} />
       </View>
@@ -35,22 +42,30 @@ export default function HomeScreen() {
   }
 
   return (
-    <View style={styles.container}>
-      <CameraView
-        style={styles.camera}
-        facing='back'
-        barcodeScannerSettings={{
-          barcodeTypes: ['codabar', 'ean13', 'qr']
-        }}
-        onBarcodeScanned={handleBarCodeScanned}
-      >
-      </CameraView>
-      {scanned && (
-        <ScannedProductOverlay barcodeData={barcodeData} onClose={() => {
-          setScanned(false);
-          setBarcodeData(null)
-        }}/>
-      )}
+    <View style={{ flex: 1 }}>
+      <SafeAreaView style={styles.containerHistory}>
+        <TouchableOpacity style={styles.buttonHistory} onPress={(() => console.log('Icon pushed'))}>
+          <MaterialIcons name="history" size={32} color="#fff" />
+        </TouchableOpacity>
+      </SafeAreaView>
+
+      <View style={styles.container}>
+        <CameraView
+          style={styles.camera}
+          facing='back' 
+          barcodeScannerSettings={{
+            barcodeTypes: ['codabar', 'ean13', 'qr']
+          }}
+          onBarcodeScanned={handleBarCodeScanned}
+        >
+        </CameraView>
+        {scanned && (
+          <ScannedProductOverlay barcodeData={barcodeData} onClose={() => {
+            setScanned(false);
+            setBarcodeData(null);
+          }}/>
+        )}
+      </View>
     </View>
   )
 }
@@ -58,7 +73,28 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  container2: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignItems: 'center'
+  },
+  containerHistory: {
+    zIndex: 99,
+    position: 'absolute',
+    right: 20,
+    top: 10
+  },
+  buttonHistory: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#62b55c',
+    width: 80,
+    height: 40,
+    borderRadius: 10
   },
   camera: {
     flex: 1
